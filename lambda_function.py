1ff6158fed6c41759ebb07a78edcf66bf0f02634
Moritz Bunse <moritz.bunse@gmail.com>
2018-01-08 14:02:58 +0100

    Detecting faces on the fly without saving on S3

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,5 @@
-import json
 import base64
 import boto3
-import datetime
-from aws_secrets import BUCKET_NAME
 
 def decode_base64(data):
     """Decode base64, padding being optional.
@@ -17,13 +14,7 @@ def decode_base64(data):
     return base64.decodebytes(data)
     
 def lambda_handler(event, context):
-    # TODO implement
-    #return decode_base64(event['picture'].split(',')[1])
     data = decode_base64(event['picture'].split(',')[1].encode('ascii'))
-    s3 = boto3.resource('s3')
     rekognition = boto3.client('rekognition','eu-west-1')
-    filename = datetime.datetime.now().isoformat() + ".png"
-    object = s3.Object(BUCKET_NAME, filename)
-    object.put(Body=data)
-    response = rekognition.detect_faces(Image={'S3Object':{'Bucket':BUCKET_NAME,'Name':filename}},Attributes=['ALL'])
+    response = rekognition.detect_faces(Image={'Bytes':data},Attributes=['ALL'])
     return response
