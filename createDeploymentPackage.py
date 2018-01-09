2896469d3ca9afa8390ff4124233f4e59712c4ea
Moritz Bunse <mbunse@hotmail.com>
2018-01-09 22:36:36 +0100

    updated deploymentpackage script to work with AMI Linux

--- createDeploymentPackage.py
+++ createDeploymentPackage.py
@@ -2,7 +2,7 @@ from zipfile import ZipFile
 from shutil import make_archive
 import os
 
-DIRNAME = "C:\\Users\\Moritz\\Anaconda3\\envs\\aws_lambda\\Lib\\site-packages"
+DIRNAME = "$VIRTUAL_ENV/lib/python3.6/site-packages"
 #make_archive("DeploymentPackage", 'zip', DIRNAME)
 with ZipFile('DeploymentPackage.zip', 'a') as myzip:
     myzip.write('lambda_function.py')
